8343a33afbd362f054881ab2899a32b7d6bd28ec
A601-03 <66713747+reiji-dat@users.noreply.github.com>
2022-01-27 14:19:07 +0900

    Initial commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    TPSCameraController tpsCamera;
+    //当たり判定
+    CharacterController cc;
+
+    // 現在の落下速度
+    float velY;
+
+    // 移動速度
+    [SerializeField]
+    float moveSpeed = 5.0f;
+    // 回転速度
+    [SerializeField] 
+    float charaRotationSpeed = 20.0f;
+    //ジャンプ力
+    [SerializeField]
+    float jumpingForce = 5.0f;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    // 移動処理
+    void Movement()
+    {
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input.Normalize();
+
+        //地面についていてジャンプしていればジャンプする
+        if (cc.isGrounded && Input.GetButton("Jump")) velY = jumpingForce;
+        //地面についてたらこれ以上落ちないようにする
+        velY += Physics.gravity.y * Time.fixedDeltaTime;
+        velY = cc.isGrounded && !Input.GetButton("Jump") ? 0.0f : velY;
+
+        //速度は最後にまとめて処理する。
+        if(input == Vector2.zero)
+        {
+            cc.Move(Vector3.up * velY * Time.fixedDeltaTime);
+        }
+        else
+        {
+            //カメラを基準に上下左右動くようにしたら良いかも？
+            cc.Move(transform.forward * Time.fixedDeltaTime * moveSpeed + Vector3.up * velY * Time.fixedDeltaTime);
+            Rotation(input);
+        }
+    }
+
+    //※以下の処理はターゲットが右にあれば右回転、左にあれば左回転という処理に置き換えられそう。
+    //回転処理
+    void Rotation(Vector2 input)
+    {
+        var targetHorizontalRtt = tpsCamera.horizontalRtt + Angle(input);
+        targetHorizontalRtt = Mathf.Repeat(targetHorizontalRtt, 360.0f);
+
+        var playerHorizontalRtt = Mathf.Repeat(transform.localEulerAngles.y, 360.0f);
+
+        //右側判定最大の値
+        var rightMax = Mathf.Repeat(targetHorizontalRtt + 180, 360);
+
+        //誤差修正
+        if (Mathf.Abs(playerHorizontalRtt - targetHorizontalRtt) <= charaRotationSpeed)
+            transform.localEulerAngles = Vector3.up * targetHorizontalRtt;
+        else if (playerHorizontalRtt != targetHorizontalRtt)
+        {
+            //目標より右の最大が大きければ
+            if (targetHorizontalRtt < rightMax)
+            {
+                //範囲が内側にあれば右向きに回転
+                if (targetHorizontalRtt < playerHorizontalRtt && rightMax > playerHorizontalRtt)
+                    transform.localEulerAngles = Vector3.up * (playerHorizontalRtt - charaRotationSpeed);
+                else
+                    transform.localEulerAngles = Vector3.up * (playerHorizontalRtt + charaRotationSpeed);
+            }
+            else
+            {
+                //目標より大きいか右の最大より小さければ右向きに回転
+                if (targetHorizontalRtt < playerHorizontalRtt || rightMax > playerHorizontalRtt)
+                    transform.localEulerAngles = Vector3.up * (playerHorizontalRtt - charaRotationSpeed);
+                else
+                    transform.localEulerAngles = Vector3.up * (playerHorizontalRtt + charaRotationSpeed);
+            }
+        }
+    }
+
+    //Mathfでは360度に対応していないため自分で作った。
+    float Angle(Vector2 v2)
+    {
+        float rad = Mathf.Atan2(v2.x, v2.y);
+        return rad * Mathf.Rad2Deg;
+    }
+}
---
+++ Assets/TPSCameraController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class TPSCameraController : MonoBehaviour
+{
+    //プレイヤーの位置
+    [SerializeField]
+    Transform player;
+
+	// プレイヤーとカメラの距離
+    [SerializeField, Min(0)]
+    float distance = 10;
+
+    //距離補正
+    [SerializeField, Min(0)]
+    float distanceCorrection = 0.1f;
+
+    // マウス感度
+    [SerializeField] 
+    float mouseSensitivity = 2.0f;
+
+    //縦の角度
+    float verticalRtt = 0.0f;
+    //横の角度
+    public float horizontalRtt = 0.0f;
+
+    //縦の下限角度
+    [SerializeField]
+    float minVerticalRtt = -60.0f;
+    //縦の上限角度
+    [SerializeField]
+    float maxVerticalRtt = 80.0f;
+
+    void FixedUpdate()
+	{
+        CameraControl();
+
+        CameraMove();
+	}
+
+    // カメラ操作
+    void CameraControl()
+    {
+        //マウスが移動した距離を取得
+        var mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        mouse *= mouseSensitivity;
+
+        //横方向にカメラを動かす
+        horizontalRtt += mouse.x;
+        //0~360以内に変換
+        horizontalRtt = Mathf.Repeat(horizontalRtt, 360.0f);
+
+        verticalRtt -= mouse.y;
+        verticalRtt = Mathf.Clamp(verticalRtt, minVerticalRtt, maxVerticalRtt);
+    }
+
+    //カメラを動かす。
+    void CameraMove()
+    {
+        //プレイヤーの位置にカメラを移動
+        transform.position = player.position;
+
+        //(マウスの上下,マウスの左右,0)
+        transform.localEulerAngles = new Vector3(verticalRtt, horizontalRtt, 0);
+
+        //プレイヤーとカメラの位置関係を調整
+        transform.Translate(Vector3.back * distance);
+
+        //後ろに行き過ぎた分前に動かす。
+        transform.Translate(Vector3.forward * (distance - RayDistance()));
+    }
+
+    /// <summary>
+    /// プレイヤーと壁の距離を求める
+    /// </summary>
+    /// <returns>プレイヤーと壁の距離</returns>
+    float RayDistance()
+    {
+        //ヒットしたらプレイヤーと当たった場所の距離を求める。
+        if (Physics.Linecast(player.position, transform.position, out var hit))
+            return Vector3.Distance(player.position, hit.point) - distanceCorrection;
+        //なければ最大値を返す。
+        else
+            return distance;
+    }
+}
